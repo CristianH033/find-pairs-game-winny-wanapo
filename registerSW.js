9666bce32240220c13a77d225bcf8f12d1545528
CristianH033 <CristianH033@users.noreply.github.com>
2024-08-08 17:31:09 +0000

    deploy: 09acd497f3743b7844167f034f613bcf1f57d433

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/find-pairs-game-winny-wanapo/sw.js', { scope: '/find-pairs-game-winny-wanapo/' })})}
